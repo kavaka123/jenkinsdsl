5c8e32c583e4b0206632b2f077414d217674f06f
kavaka123 <46238960+kavaka123@users.noreply.github.com>
2020-03-26 18:41:34 +0530

    Update antjobs.groovy
    added docker container and delivery pipeline view code

--- dslscripts/antjobs.groovy
+++ dslscripts/antjobs.groovy
@@ -1,4 +1,4 @@
-job('antbuild') {
+job('ant-build') {
     scm {
         github('tetradev01/demoapp', 'master')
     }
@@ -7,21 +7,34 @@ job('antbuild') {
         ant {
             prop('version', 'dev')
             buildFile('build.xml')
-            antInstallation('Ant 1.9.3')
+            antInstallation('local-ant')
         }
     }
 
     publishers {
-        downstream 'deploy', 'SUCCESS'
+        downstream 'ant-deploy', 'SUCCESS'
     }
 }
 
-job('deploy') {
+job('ant-deploy') {
     description 'Deploy app to the demo application server'
     /*
      * configuring ssh plugin to run docker commands
      */
     steps{
-             shell 'sshpass -p \'123456\' scp /var/lib/jenkins/workspace/antbuild/build/demoapp-dev.war release@10.12.108.11:/opt/tomcat/webapps/'
+       //      shell 'sshpass -p \'123456\' scp /var/lib/jenkins/workspace/antbuild/build/demoapp-dev.war release@10.12.108.11:/opt/tomcat/webapps/'
+      batchFile('copy "C:\\Program Files (x86)\\Jenkins\\workspace\\ant-build\\build\\demoapp-dev.war" .')
+      batchFile('docker rm -f %JOB_NAME% && docker run -d -p 8888:8080 --name %JOB_NAME% tomcat && docker cp demoapp-dev.war %JOB_NAME%:/usr/local/tomcat/webapps/')
       }
 }
+
+
+deliveryPipelineView('ant job delivery pipeline') {
+    showAggregatedPipeline true
+    enableManualTriggers true
+    pipelineInstances 3
+    pipelines {
+        component('ant job delivery pipeline', 'ant-build')
+    }
+}
+  
